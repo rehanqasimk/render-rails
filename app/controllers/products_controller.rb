ebe16ddee65fe38f297b229cb74e46f7fea72108
rehanqasimk <qkrehan@gmail.com>
2025-04-10 16:31:49 +0500

    prudcts crud added

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,48 @@
 class ProductsController < ApplicationController
+  before_action :set_product, only: [ :show, :update, :destroy ]
+
   def index
     @products = Product.all
     render json: @products
+  end
+
+  def show
+    render json: @product
+  end
+
+  def create
+    @product = Product.new(product_params)
+
+    if @product.save
+      render json: @product, status: :created
+    else
+      render json: { errors: @product.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if @product.update(product_params)
+      render json: @product
+    else
+      render json: { errors: @product.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @product.destroy
+    head :no_content
+  end
+
+  private
+
+  def set_product
+    @product = Product.find(params[:id])
+  rescue ActiveRecord::RecordNotFound
+    render json: { error: "Product not found" }, status: :not_found
+  end
+
+  def product_params
+    params.require(:product).permit(:name, :description, :price, :category)
+    # Adjust the permitted attributes according to your Product model
   end
 end
